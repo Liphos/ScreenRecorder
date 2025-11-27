696667570593f40141d44a31a3ce1b83ead4d22c
Liphos <levyguillaume10@yahoo.fr>
2025-11-27 15:44:12 +0100

    fix: restore tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -24,53 +24,51 @@ def test_screen_recording():
     manager.run_until_stop(timeout=100)
 
 
-# def test_input_recording():
-#     manager = Manager(
-#         [
-#             KeyboardRecording(),
-#             MouseRecording(),
-#             StopRecording(),
-#         ],
-#         path_output="./screenshots/test/",
-#         print_results=False,
-#     )
-#     manager.run_until_stop(timeout=10)
+def test_input_recording():
+    manager = Manager(
+        [
+            KeyboardRecording(),
+            MouseRecording(),
+            StopRecording(),
+        ],
+        path_output="./screenshots/test/",
+        print_results=False,
+    )
+    manager.run_until_stop(timeout=10)
 
 
-# def test_gamepad_recording():
-#     manager = Manager(
-#         [
-#             GamepadRecording(),
-#         ],
-#         path_output="./screenshots/test/",
-#         print_results=False,
-#     )
-#     manager.run_until_stop(timeout=10)
+def test_gamepad_recording():
+    manager = Manager(
+        [
+            GamepadRecording(),
+        ],
+        path_output="./screenshots/test/",
+        print_results=False,
+    )
+    manager.run_until_stop(timeout=10)
 
 
-# def test_external_stop():
-#     manager = Manager(
-#         [
-#             ScreenRecording(n_processes=3, aimed_fps=10, compression_rate=6, max_screenshots=1000),
-#         ],
-#         path_output="./screenshots/test/",
-#         print_results=False,
-#     )
-#     manager.run_until_stop(timeout=10)
+def test_external_stop():
+    manager = Manager(
+        [
+            ScreenRecording(n_processes=3, aimed_fps=10, compression_rate=6, max_screenshots=1000),
+        ],
+        path_output="./screenshots/test/",
+        print_results=False,
+    )
+    manager.run_until_stop(timeout=10)
 
 
-# def test_combined_recording():
-#     manager = Manager(
-#         [
-#             ScreenRecording(
-#                 n_processes=3, aimed_fps=10, compression_rate=6, max_screenshots=1000
-#             ),
-#             KeyboardRecording(),
-#             MouseRecording(),
-#             StopRecording(),
-#             GamepadRecording(),
-#         ],
-#         path_output="./screenshots/test/",
-#         print_results=False,
-#     )
-#     manager.run_until_stop(timeout=10)
+def test_combined_recording():
+    manager = Manager(
+        [
+            ScreenRecording(n_processes=3, aimed_fps=10, compression_rate=6, max_screenshots=1000),
+            KeyboardRecording(),
+            MouseRecording(),
+            StopRecording(),
+            GamepadRecording(),
+        ],
+        path_output="./screenshots/test/",
+        print_results=False,
+    )
+    manager.run_until_stop(timeout=10)
